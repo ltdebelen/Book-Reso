a1167e6aaf312066137b725bc17dfa446909ab9c
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-01-25 12:02:07 -0600

    Added books and overflow behavior

--- assets/js/books.js
+++ assets/js/books.js
@@ -18,5 +18,45 @@ const books = [
     title: "Wild",
     author: "Cheryl Strayed",
     category: "travel"
+  },
+  {
+    title: "Becoming",
+    author: "Michelle Obama",
+    category: "mindset"
+  },
+  {
+    title: "Rich Dad Poor Dad",
+    author: "Robert Kiyosaki",
+    category: "finance"
+  },
+  {
+    title: "Boundless",
+    author: "Ben Greenfield",
+    category: "fitness"
+  },
+  {
+    title: "Wild",
+    author: "Cheryl Strayed",
+    category: "travel"
+  },
+  {
+    title: "Becoming",
+    author: "Michelle Obama",
+    category: "mindset"
+  },
+  {
+    title: "Rich Dad Poor Dad",
+    author: "Robert Kiyosaki",
+    category: "finance"
+  },
+  {
+    title: "Boundless",
+    author: "Ben Greenfield",
+    category: "fitness"
+  },
+  {
+    title: "Wild",
+    author: "Cheryl Strayed",
+    category: "travel"
   }
 ];
