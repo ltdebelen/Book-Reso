dfa8f26a5b1a8c3c0b8ec8a104faa1f750d4c65b
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-01-25 11:19:42 -0600

    Adding map section

--- assets/js/books.js
+++ assets/js/books.js
@@ -18,25 +18,5 @@ const books = [
     title: "Wild",
     author: "Cheryl Strayed",
     category: "travel"
-  },
-  {
-    title: "Make it stick",
-    author: "Peter Brown",
-    category: "learning"
-  },
-  {
-    title: "Atomic Habits",
-    author: "James Clear",
-    category: "mindset"
-  },
-  {
-    title: "The Richest Man in Babylon",
-    author: "Samuel Clason",
-    category: "finance"
-  },
-  {
-    title: "A Mind for Numbers",
-    author: "Barbara Oakley",
-    category: "learning"
   }
 ];
