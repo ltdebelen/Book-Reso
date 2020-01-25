9aa0794b59a15d0c44067b6d76822bcde6f270af
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-01-25 01:15:39 -0600

    Created functions to display Google API info to HTML

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,100 @@
-// Modal Function
-$(function() {
+$(document).ready(function() {
+  const books = [
+    {
+      title: "Becoming",
+      author: "Michelle Obama",
+      category: "mindset"
+    },
+    {
+      title: "Rich Dad Poor Dad",
+      author: "Robert Kiyosaki",
+      category: "finance"
+    },
+    {
+      title: "Boundless",
+      author: "Ben Greenfield",
+      category: "fitness"
+    },
+    {
+      title: "The Alchemist",
+      author: "Paulo Coelho",
+      category: "travel"
+    },
+    {
+      title: "Make it stick",
+      author: "Peter Brown",
+      category: "learning"
+    }
+  ];
+
+  books.forEach(book => {
+    searchBook(book);
+  });
+
+  function searchBook(book) {
+    const API_KEY = "AIzaSyBXnJxhMnxkR9s56HdpqIoGb62ATyP3nDk";
+    let base_url = "https://www.googleapis.com/books/v1/volumes?q=";
+
+    let title = book.title;
+    let titleArr = title.split(" ");
+    let titleParam = titleArr.join("+");
+
+    let author = book.author;
+    let authorArr = author.split(" ");
+    let authorParam = authorArr.join("+");
+
+    let newURL =
+      base_url +
+      "intitle:" +
+      titleParam +
+      "+" +
+      "inauthor:" +
+      authorParam +
+      "&key=" +
+      API_KEY;
+    console.log(newURL);
+
+    $.ajax({
+      url: newURL,
+      method: "GET"
+    }).then(function(response) {
+      const title = response.items[0].volumeInfo.title;
+      const imgurl = response.items[0].volumeInfo.imageLinks.thumbnail;
+      const category = book.category;
+
+      createBookCard(title, imgurl, category);
+    });
+  }
+
+  function createBookCard(title, imgurl, category) {
+    console.log(title);
+    console.log(imgurl);
+    let bookCardHTML = `
+    <div class="column book-item ${category}">
+      <div class="card">
+        <header class="card-header">
+          <p class="has-text-centered card-header-title">
+            ${title}
+          </p>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            <img src="${imgurl}" alt="${title}" class="book-image">
+          </div>
+        </div>
+        <footer class="card-footer">
+          <a href="#" class="card-footer-item">Save</a>
+          <a href="#" class="card-footer-item">Edit</a>
+          <a href="#" class="card-footer-item">Delete</a>
+        </footer>
+      </div>
+    </div>
+    `;
+
+    $(".book-menu-items .columns").append(bookCardHTML);
+  }
+
+  // Modal Function
   $(".modal-background, .modal-close").click(function() {
     $("html").removeClass("is-clipped");
     $(this)
@@ -13,7 +108,7 @@ $(function() {
   });
 
   // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(
+  let $navbarBurgers = Array.prototype.slice.call(
     document.querySelectorAll(".navbar-burger"),
     0
   );
@@ -24,8 +119,8 @@ $(function() {
     $navbarBurgers.forEach(function($el) {
       $el.addEventListener("click", () => {
         // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
+        let target = $el.dataset.target;
+        let $target = document.getElementById(target);
 
         // Toggle the class on both the "navbar-burger" and the "navbar-menu"
         $el.classList.toggle("is-active");
@@ -33,16 +128,16 @@ $(function() {
       });
     });
   }
-});
 
-// Isotope Book Filter Function
-$(".book-menu-items").isotope({
-  itemSelector: ".book-item"
-});
-$(".book-menu a").click(function() {
-  let selector = $(this).attr("data-filter");
+  // Isotope Book Filter Function
   $(".book-menu-items").isotope({
-    filter: selector
+    itemSelector: ".book-item"
   });
-  return false;
+  $(".book-menu a").click(function() {
+    let selector = $(this).attr("data-filter");
+    $(".book-menu-items").isotope({
+      filter: selector
+    });
+    return false;
+  });
 });
