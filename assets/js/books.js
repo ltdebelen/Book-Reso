bcdc768ddfb92c44053d8793fcb7268df1c6ab53
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-01-26 17:47:51 -0600

    Fixing mobile responsiveness

--- assets/js/books.js
+++ assets/js/books.js
@@ -1,43 +1,18 @@
 const books = [
   {
-    title: "Becoming",
-    author: "Michelle Obama",
+    title: "Grit",
+    author: "Angela Duckworth",
     category: "mindset"
   },
   {
-    title: "Rich Dad Poor Dad",
-    author: "Robert Kiyosaki",
-    category: "finance"
-  },
-  {
-    title: "Boundless",
-    author: "Ben Greenfield",
-    category: "fitness"
-  },
-  {
-    title: "Wild",
-    author: "Cheryl Strayed",
-    category: "travel"
-  },
-  {
-    title: "Becoming",
-    author: "Michelle Obama",
+    title: "The Power of Now",
+    author: "Eckhart Tolle",
     category: "mindset"
   },
   {
-    title: "Rich Dad Poor Dad",
-    author: "Robert Kiyosaki",
-    category: "finance"
-  },
-  {
-    title: "Boundless",
-    author: "Ben Greenfield",
-    category: "fitness"
-  },
-  {
-    title: "Wild",
-    author: "Cheryl Strayed",
-    category: "travel"
+    title: "Atomic Habits",
+    author: "James Clear",
+    category: "mindset"
   },
   {
     title: "Becoming",
@@ -50,13 +25,78 @@ const books = [
     category: "finance"
   },
   {
+    title: "Think and Grow Rich",
+    author: "Napoleon Hill",
+    category: "finance"
+  },
+  {
+    title: "Money A to Z",
+    author: "Scott Alan Turner",
+    category: "finance"
+  },
+  {
+    title: "The Millionaire Next Door",
+    author: "Thomas Stanley",
+    category: "finance"
+  },
+  {
     title: "Boundless",
     author: "Ben Greenfield",
     category: "fitness"
   },
   {
+    title: "The Power of Habit",
+    author: "Charles Duhigg",
+    category: "fitness"
+  },
+  {
+    title: "Spark",
+    author: "John Ratey",
+    category: "fitness"
+  },
+  {
+    title: "Sleep Smarter",
+    author: "Shawn Stevenson",
+    category: "fitness"
+  },
+  {
+    title: "The Rings of Saturn",
+    author: "W.G. Sebald",
+    category: "travel"
+  },
+  {
+    title: "Vagabonding",
+    author: "Rolf Potts",
+    category: "travel"
+  },
+  {
     title: "Wild",
     author: "Cheryl Strayed",
     category: "travel"
+  },
+  {
+    title: "The Art of Travel",
+    author: "Alain De Botton",
+    category: "travel"
+  },
+  {
+    title: "Make it stick",
+    author: "Peter Brown",
+    category: "learning"
+  },
+  {
+    title: "A Mind for Numbers",
+    author: "Barbara Oakley",
+    category: "learning"
+  },
+  {
+    title: "The Magic of Math",
+    author: "Arthur Benjamin",
+    category: "learning"
+  },
+  {
+    title: "Clean Coe",
+    author: "Robert Cecil Martin",
+    category: "learning"
   }
 ];
